81b39d4985ffb2506bff8aa7450000332c02629f
Aleksey Kuznetsov <avkuznetsov@mmip.ru>
2025-12-31 21:42:01 +0300

    init commit

---
+++ selenium-lab3.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+import time
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+time.sleep(5)

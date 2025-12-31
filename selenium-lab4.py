b4a287789f5bad3a4058db19e9c9ee3d61c96951
Aleksey Kuznetsov <avkuznetsov@mmip.ru>
2025-12-31 22:05:51 +0300

    init commit

---
+++ selenium-lab4.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+import time
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.get('https://mail.mmip.ru')
+
+driver.find_element("id", "rcmloginsubmit")
+
+driver.find_element("id", "rcmloginsubmit").click()
+
+time.sleep(5)
+
